8e6f2941a07163adf691d99e6607fa6a3c6965ea
Thapsaenchai Chasanguan <zazavc22@gmail.com>
2021-04-14 13:22:26 +0700

    Update Last_New6

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,19 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] ='mykey'
 app.config['DEBUG'] = True
-
+'''
 from app.api_covid19 import show_covid
 from app.api_PM25_Thailand import show_pm25
 from app.api_Weather_Thailand import show_weather, show_weatherF
 #from app.all_link_url import *
-from app.test_api import Now_AQI
+'''
+from app.test_api import *
 
 
 @app.route("/", methods=(['GET', 'POST']))
 @app.route("/index", methods=(['GET', 'POST']))
 def Show_Index():
+    
     '''
     arguments_covid = show_covid(Covid19_url_TH,Covid19_url_World)
     arguments_pm25 = show_pm25(PM_25_url_TH)
@@ -24,7 +26,8 @@ def Show_Index():
     return render_template("index.html",Value_Covid=arguments_covid, Value_PM25=arguments_pm25, 
     Value_WForcast=arguments_WeatherForecast7Days, Value_WToday=arguments_WeatherToday)
     '''
-    return render_template('test.html',Value_PM25=Now_AQI)
+    arguments_pm25 = show_pm25(PM_25_url_TH)
+    return render_template('test.html',Value_PM25=arguments_pm25)
 
 if __name__ == '__main__':
     #app.debug = True
