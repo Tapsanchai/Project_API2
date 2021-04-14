f78cad04d4bd8b1a173892aae2ec93db3764375a
Thapsaenchai Chasanguan <zazavc22@gmail.com>
2021-04-14 15:23:27 +0700

    Upload Last_New14

--- app/api_Weather_Thailand.py
+++ app/api_Weather_Thailand.py
@@ -4,7 +4,7 @@ import json
 WeatherToday_url_TH = "https://data.tmd.go.th/api/WeatherToday/V1/?type=json"
 WeatherForecast7Days_url_TH = "http://data.tmd.go.th/api/WeatherForecast7Days/V1/?type=json"
 
-def show_weather(url_w_th):
+def show_weather(url_w_th, url_wf_th):
     # WeatherToday
     response1 = requests.get(url_w_th)
     result1 = json.loads(response1.text)
@@ -14,9 +14,7 @@ def show_weather(url_w_th):
     Now_City = result1['Stations'][84]['StationNameTh']
     Now_Temperature = result1['Stations'][84]['Observe']['Temperature']['Value']
     Data_WeatherToday = {"Datetime": Now_Datetime,"City": Now_City, "Tem": Now_Temperature}
-    return Data_WeatherToday
 
-def show_weatherF(url_wf_th):
     # WeatherForecast7Days
     response2 = requests.get(url_wf_th)
     result2 = json.loads(response2.text)
@@ -26,8 +24,11 @@ def show_weatherF(url_wf_th):
     Forecast_City = result2['Provinces'][36]['ProvinceNameTh']
     Forecast_Datetime = result2['Provinces'][36]['SevenDaysForecast'][0]['Date']
     Data_WeatherForecast7Days = {"Forecast": Forecast, "Forecast_City": Forecast_City, "Forecast_Date": Forecast_Datetime}
-    return Data_WeatherForecast7Days
+    
+    json_of_value_Weather = {"Data_WeatherToday": Data_WeatherToday, "Data_WeatherForecast7Days": Data_WeatherForecast7Days}
+
+    return json_of_value_Weather
+
 
 if __name__ == '__main__':
     show_weather()
-    show_weatherF()
