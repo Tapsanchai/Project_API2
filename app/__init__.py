c106eef86c7ac0c6fed9dd396e80fc120c5259bc
Thapsaenchai Chasanguan <zazavc22@gmail.com>
2021-04-14 13:07:22 +0700

    Update Last_New4

--- app/__init__.py
+++ app/__init__.py
@@ -8,20 +8,24 @@ app.config['DEBUG'] = True
 from app.api_covid19 import show_covid
 from app.api_PM25_Thailand import show_pm25
 from app.api_Weather_Thailand import show_weather, show_weatherF
-from app.all_link_url import *
+#from app.all_link_url import *
+from app.test_api import Now_AQI
+
 
 @app.route("/", methods=(['GET', 'POST']))
 @app.route("/index", methods=(['GET', 'POST']))
 def Show_Index():
-
+    '''
     arguments_covid = show_covid(Covid19_url_TH,Covid19_url_World)
     arguments_pm25 = show_pm25(PM_25_url_TH)
     arguments_WeatherToday = show_weather(WeatherToday_url_TH)
     arguments_WeatherForecast7Days = show_weatherF(WeatherForecast7Days_url_TH)
-    '''
+    
     return render_template("index.html",Value_Covid=arguments_covid, Value_PM25=arguments_pm25, 
     Value_WForcast=arguments_WeatherForecast7Days, Value_WToday=arguments_WeatherToday)
     '''
+    return render_template('test.html',Value_PM25=Now_AQI)
+
 if __name__ == '__main__':
     #app.debug = True
     #app.run(host='localhost', port=55)
---
+++ app/test_api.py
@@ -0,0 +1,7 @@
+import requests
+import json 
+
+PM_25_url_TH = "http://api.airvisual.com/v2/city?city=Chiang Rai&state=Chiang Rai&country=Thailand&key=eb400bfd-3588-4482-9411-5fd3d7d434a8"
+response = requests.get(PM_25_url_TH)
+result = json.loads(response.text)
+Now_AQI = result['data']['current']['pollution']['aqius']
