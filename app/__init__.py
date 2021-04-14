11a66c3f2b49453d288a7d6c1bae00bb9ac88d37
Thapsaenchai Chasanguan <zazavc22@gmail.com>
2021-04-14 15:03:26 +0700

    Update Last_New13

--- app/__init__.py
+++ app/__init__.py
@@ -15,15 +15,16 @@ def Show_Index():
     
 
     arguments_pm25 = show_pm25(PM_25_url_TH)
-    '''
-    arguments_covid = show_covid(Covid19_url_TH,Covid19_url_World)
     arguments_WeatherToday = show_weather(WeatherToday_url_TH)
     arguments_WeatherForecast7Days = show_weatherF(WeatherForecast7Days_url_TH)
-
+    '''
+    arguments_covid = show_covid(Covid19_url_TH,Covid19_url_World))
+    
     return render_template("test.html",Value_Covid=arguments_covid, Value_PM25=arguments_pm25, 
     Value_WForcast=arguments_WeatherForecast7Days, Value_WToday=arguments_WeatherToday)
     '''
-    return render_template("test.html",Value_PM25=arguments_pm25)
+    return render_template("test.html",Value_PM25=arguments_pm25, Value_WForcast=arguments_WeatherForecast7Days, 
+    Value_WToday=arguments_WeatherToday )
 
 
 if __name__ == '__main__':
