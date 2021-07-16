c0e8820813f9273cd48d6204d627606ffda139d7
Thapsaenchai Chasanguan <zazavc22@gmail.com>
2021-07-16 20:52:30 +0700

    New Update | confix Covid19 TH

--- app/api_covid19.py
+++ app/api_covid19.py
@@ -1,7 +1,7 @@
 # from app import __init__
 import json
 import requests
-
+             
 Covid19_url_TH ="https://covid19.th-stat.com/json/covid19v2/getTodayCases.json"
 Covid19_url_World = "https://covid-19-world-data-by-zt.p.rapidapi.com/GetTotalCounts"
 
@@ -9,12 +9,10 @@ def show_covid(url_th, url_world):
     # Covid19 today[thailand]
     response_covid19 = requests.get(url_th,timeout=(100,100))
 
-    response_covid19.encoding = "utf-8"
-    re_dom =json.dumps(response_covid19.text)
-    
-    result_covid19 = json.loads(re_dom)
+    response_covid19.encoding = "utf-8"    
+    result_covid19 = json.loads(response_covid19.text)
     Now_Covid19 = result_covid19
-
+    print('now_covid19 = ', Now_Covid19, '-->', type(Now_Covid19))
     # Covid19 All World 
     headers = {
         'x-rapidapi-key': "06cc87bd05msh5f606bb8f4a703ap1df09fjsn7f38f410331e",
@@ -25,8 +23,8 @@ def show_covid(url_th, url_world):
     Now_Covid19_all = json.loads(response_covid_World.text)
     Data_Covid19_all = Now_Covid19_all['data'][0]
     json_of_value_covid = {"Now_Covid19": Now_Covid19, "Data_Covid19_all": Data_Covid19_all}
-    return(json_of_value_covid)
+    return json_of_value_covid
 
 # if __name__ == '__main__':
-#     show_covid()
+#     show_covid(Covid19_url_TH, Covid19_url_World)
     
